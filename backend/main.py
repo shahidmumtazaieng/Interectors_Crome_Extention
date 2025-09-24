7db42bd4f8492fdee21c0cdbf93ef561507e0e53
Shahid Mumtaz <105332910+shahidmumtazaieng@users.noreply.github.com>
2025-09-23 23:49:55 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     import os
     
     # Get port from environment variable or default to 8000
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8001))
     
     # Run the FastAPI app with Uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=port, log_level="info")
+
+    uvicorn.run("app:app", host="https://interectorscromeextention-ia6dt2a3vdvrbrrzyx3gbn.streamlit.app/", port=port, log_level="info")
