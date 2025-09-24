896cf26d8ab54f1d2a171c4baa4aa471ee2bf9ec
Shahid Mumtaz <105332910+shahidmumtazaieng@users.noreply.github.com>
2025-09-23 22:15:03 -0700

    Update streamlit_app.py

--- backend/streamlit_app.py
+++ backend/streamlit_app.py
@@ -1,8 +1,9 @@
-# This is the main Streamlit app file that will be run when deployed to Streamlit Cloud
-# It imports and runs the UI component
-
-import os
-import sys
-
-
+# Check if we should run in API mode
+if len(sys.argv) > 1 and sys.argv[1] == "api":
+    # Set environment variable to indicate API mode
+    os.environ["STREAMLIT_API_MODE"] = "1"
+    # Import the API part
+    import streamlit_ui
+else:
+    # Run in UI mode
     import streamlit_ui
