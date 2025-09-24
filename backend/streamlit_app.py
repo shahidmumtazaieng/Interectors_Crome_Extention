23a9a299a09d5c2a8e658a7e2b159228e2226f77
Shahid Mumtaz <105332910+shahidmumtazaieng@users.noreply.github.com>
2025-09-23 22:13:03 -0700

    Update streamlit_app.py

--- backend/streamlit_app.py
+++ backend/streamlit_app.py
@@ -4,12 +4,5 @@
 import os
 import sys
 
-# Check if we should run in API mode
-if len(sys.argv) > 1 and sys.argv[1] == "api":
-    # Set environment variable to indicate API mode
-    os.environ["STREAMLIT_API_MODE"] = "1"
-    # Import the API part
-    import streamlit_ui
-else:
-    # Run in UI mode
+
     import streamlit_ui
